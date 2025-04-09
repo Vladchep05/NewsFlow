9611ac54b4737dabaac791337856a57d18061168
sanya <vladcepel05@gmail.com>
2025-04-10 02:18:11 +0700

    переделал форму регестрации и форму входа (стиль)

---
+++ static/js/login.js
@@ -0,0 +1,51 @@
+particlesJS("particles-js", {
+            "particles": {
+                "number": {
+                    "value": 80,
+                    "density": {
+                        "enable": true,
+                        "value_area": 800
+                    }
+                },
+                "color": {
+                    "value": "#00ffe0"
+                },
+                "shape": {
+                    "type": "circle"
+                },
+                "opacity": {
+                    "value": 0.4,
+                    "random": true
+                },
+                "size": {
+                    "value": 3,
+                    "random": true
+                },
+                "line_linked": {
+                    "enable": true,
+                    "distance": 150,
+                    "color": "#00ffe0",
+                    "opacity": 0.3,
+                    "width": 1
+                },
+                "move": {
+                    "enable": true,
+                    "speed": 1,
+                    "random": true,
+                    "out_mode": "out"
+                }
+            },
+            "interactivity": {
+                "events": {
+                    "onhover": {
+                        "enable": true,
+                        "mode": "grab"
+                    },
+                    "onclick": {
+                        "enable": true,
+                        "mode": "repulse"
+                    }
+                }
+            },
+            "retina_detect": true
+        });
---
+++ static/js/registration.js
@@ -0,0 +1,4 @@
+// Плавное появление страницы
+window.addEventListener("DOMContentLoaded", () => {
+    document.body.classList.add("loaded");
+});
